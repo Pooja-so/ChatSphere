fa614374e710eb5068869ef16e704036090fddf2
Pooja Singh <Riroro137@gmail.com>
2024-03-02 18:54:31 +0530

    UI Design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,14 @@
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS chat bubble component" />
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Message;
---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,22 @@
+import Message from "./Message";
+
+const Messages = () => {
+  return (
+    // overflow-auto gives scrollbar 
+    <div className="px-4 flex-1 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+export default Messages;
